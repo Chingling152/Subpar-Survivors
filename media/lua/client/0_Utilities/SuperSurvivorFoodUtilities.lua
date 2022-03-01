d18dc3b65906dbfa2267ff5f190be701561a5ac5
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2022-03-01 13:44:49 -0300

    fix search food when language is not english
    + Adds fish/pepper search
    * Fixes not searching for canned food
    * Fixes search canned food
    * Fixes search food bug and 
    - Remove Pepper from ExcludedFoods

--- media/lua/client/0_Utilities/SuperSurvivorFoodUtilities.lua
+++ media/lua/client/0_Utilities/SuperSurvivorFoodUtilities.lua
@@ -1,11 +1,12 @@
 -- TODO: add berries, mushrooms and medicinal plants
-FoodsToExlude = {"Bleach", "Cigarettes", "HCCigar", "Antibiotics", "Teabag2", "Salt", "Pepper", "EggCarton"}
+FoodsToExlude = {"Bleach", "Cigarettes", "HCCigar", "Antibiotics", "Teabag2", "Salt", "EggCarton"}
 
 local FoodScores = {
     Cooked = 5,
     Fresh = 4,
     FruitsAndVegetables = 3,
-    Fish = 3, --not implemented
+    Fish = 3,
+    Pepper = 3,
     OpenCanned = 2,
     Canned = 1,
     Alchool = 0,
@@ -24,7 +25,7 @@ function isCanOpen(thisFood)
         return false
     end
 
-    return string.match(thisFood:getDisplayName(), "Open")
+    return string.match(thisFood:getType() .. thisFood:getDisplayName(), "Open")
 end
 
 function isDogFood(thisFood)
@@ -32,7 +33,7 @@ function isDogFood(thisFood)
         return false
     end
 
-    return string.match(thisFood:getDisplayName(), "Dog Food")
+    return string.match(thisFood:getType() .. thisFood:getDisplayName(), "Dogfood")
 end
 
 function isCanned(thisFood)
@@ -40,9 +41,10 @@ function isCanned(thisFood)
         return false 
     end
 
-    return string.match(thisFood:getType() .. thisFood:getDisplayName(), "Canned")
-end
+    local fullName = thisFood:getType() .. thisFood:getDisplayName()
 
+    return string.match(fullName,"Canned") or string.match(fullName,"Tin");
+end
 
 function FindAndReturnFood(thisItemContainer)
     if (not thisItemContainer) then
@@ -62,70 +64,105 @@ function FindAndReturnFood(thisItemContainer)
     return nil
 end
 
+local function scoreLog(text)
+    if(false) then
+        print(text);
+    end
+end
+
 function GetFoodScore(item)
     local Score = 1.0
-
+    
+    scoreLog("-----" .. item:getType() .. " : " .. item:getDisplayName() .. "-----")
     if (item:getUnhappyChange() > 0) then
         Score = Score - math.floor(item:getUnhappyChange() / (item:getHungerChange() * -10.0))
+        scoreLog("has unhapy change : " .. item:getUnhappyChange() .. " Score : " .. tostring(Score))
     elseif (item:getUnhappyChange() < 0) then
         Score = Score + 1
+        scoreLog("Score : " .. tostring(Score))
     end
 
     if (item:getBoredomChange() > 0) then
         Score = Score - math.floor(item:getBoredomChange() / (item:getHungerChange() * -10.0) / 2.0)
+        scoreLog("has boredom change : " .. item:getBoredomChange() .. " Score : " .. tostring(Score))
     elseif (item:getBoredomChange() < 0) then
         Score = Score + 1
     end
 
     if (item:isFresh()) then
         Score = Score + FoodScores.Fresh
+        scoreLog("is fresh! Score : " .. tostring(Score))
     elseif (item:IsRotten()) then
         Score = Score + FoodScores.Rotten
+        scoreLog("is rotten! Score : " .. tostring(Score))
     end
 
     if (item:isAlcoholic()) then
         Score = Score + FoodScores.Alchool
+        scoreLog("is alcoholic! Score : " .. tostring(Score))
     end
     if (item:isSpice()) then
         Score = Score + FoodScores.Spice
+        scoreLog("is spice! Score : " .. tostring(Score))
     end
-
-    if not (item:isCooked()) then
+    
+    if (item:isbDangerousUncooked()) and not (item:isCooked()) then
         Score = Score + FoodScores.Raw
+        scoreLog("is raw! Score : " .. tostring(Score))
     end
-
-    if (item:isbDangerousUncooked()) then
-        Score = Score + FoodScores.Burned
-    end
-
+        
     local FoodType = item:getFoodType()
-    if (FoodType == "NoExplicit") or (FoodType == nil) or (tostring(FoodType) == "nil") then
 
+    if FoodType then
+        scoreLog("Food Type : " .. FoodType)
+    else
+        scoreLog("No Food Type")
+    end
+
+    if (FoodType == "NoExplicit") or (FoodType == nil) or (tostring(FoodType) == "nil") then   
         if isCanned(item) and isCanOpen(item) then
             Score = Score + FoodScores.OpenCanned
+            scoreLog("is canned and opened! Score : " .. tostring(Score))
         elseif isCanned(item) then
             Score = Score + FoodScores.Canned
+            scoreLog("is canned! Score : " .. tostring(Score))
         elseif isDogFood(item) then
             Score = Score + FoodScores.DogFood
+            scoreLog("is dog food! Score : " .. tostring(Score))
         elseif (item:getHungerChange()) == nil or (item:getHungerChange() == 0) then
             Score = Score + FoodScores.Unknown
+            scoreLog("is unknown! Score : " .. tostring(Score))
         end 
         if (item:isCooked()) then
             Score = Score + FoodScores.Cooked
+            scoreLog("is cooked! Score : " .. tostring(Score))
         end
     elseif (FoodType == "Fruits") or (FoodType == "Vegetables") then
         Score = Score + FoodScores.FruitsAndVegetables
+        scoreLog("is fruit/vegetable! Score : " .. tostring(Score))
     elseif (FoodType == "Pasta") or (FoodType == "Rice") then
         Score = Score + FoodScores.DryGoods
+        scoreLog("is dry! Score : " .. tostring(Score))
     elseif ((FoodType == "Egg") or (FoodType == "Meat")) or item:isIsCookable() then
         if (item:isCooked()) then
             Score = Score + 2
+            scoreLog("is egg/meat cooked! Score : " .. tostring(Score))
         end
     elseif (FoodType == "Coffee") then
         Score = Score + FoodScores.Coffee
+        scoreLog("is coffee! Score : " .. tostring(Score))
+    elseif (FoodType == "Pepper") then
+        Score = Score + FoodScores.Pepper
+        scoreLog("is pepper! Score : " .. tostring(Score))
     else
         Score = Score + FoodScores.Unknown
-        print("Unknown food type " .. FoodType)
+        scoreLog("is unknown! Score : " .. tostring(Score))
+    end
+
+    if FoodType then
+        scoreLog("-----" .. FoodType .. " : " .. item:getType() .. " " .. item:getDisplayName() .. " Score : " .. tostring(Score) .. "-----")
+    else
+        scoreLog("-----" .. item:getType() .. " " .. item:getDisplayName() .. " Score : " .. tostring(Score) .. "-----")
     end
 
     return Score
@@ -147,7 +184,6 @@ function FindAndReturnBestFoodOnFloor(sq, survivor)
     end
 
     items = sq:getWorldObjects()
-    -- print("Checking " .. tostring(items:size()) .. " world objects.")
     for j = 0, items:size() - 1 do
         if (items:get(j):getItem()) then
             local item = items:get(j):getItem()
@@ -155,8 +191,6 @@ function FindAndReturnBestFoodOnFloor(sq, survivor)
                 (not has_value(FoodsToExlude, item:getType())) then
                 local Score = GetFoodScore(item)
 
-                -- ContainerItemsScore[i] = Score
-                -- print("BestFood Floor " .. item:getDisplayName() .. ": " .. tostring(Score))
                 if Score > BestScore then
                     BestFood = item
                     BestScore = Score
